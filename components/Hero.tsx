321edb9b1d160661e6b052f4a4ae25956f2683a9
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-10-06 14:41:12 +0200

    added the picture

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <div className="hero">
           <div className="flex-1 pt-36 padding-x">
             <h1 className="hero__title">
-              Doing Fitness doesn't need to be complicated
+              Fitness doesn't need to be complicated
             </h1>
     
             <p className="hero__subtitle">
@@ -25,7 +25,13 @@ const Hero = () => {
               handleClick={handleScroll}
             />
           </div>
-         
+          <div className="xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
+        <div className="relative xl:w-full w-[90%] xl:h-full h-[590px] z-0">
+          <Image src="/ManPicture.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden" />
+      </div>
         </div>
       );
 }
